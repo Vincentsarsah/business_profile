33f0dea81f032cea56412ce6c0cc21fffa8902a6
Vincentsarsah <69487577+Vincentsarsah@users.noreply.github.com>
2021-02-17 04:34:58 -0800

    update of homepage

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Phones from '../Phone/Phone1'
+import Computers from './../Computers/Computers';
 
 function Homepage() {
     return (
         <div>
             <Phones />
+            <Computers/>
         </div>
     )  
   }
